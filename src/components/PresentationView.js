16b98c6777786b74a5064f230d53f132514a8fa9
Juhani Åhman <juhani.ahman@digitalentshelsinki.fi>
2018-02-26 12:26:25 +0200

    Add d3 test to presentation.

--- src/components/PresentationView.js
+++ src/components/PresentationView.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as d3 from 'd3';
 
-export default class PresentationView extends React.Component {
-
-  render() {
-    return (
-      <div className="presentation-view">Nothing here yet!</div>
-    )
+class Circles extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderCircles = this.renderCircles.bind(this);
   }
 
-};
+  componentDidMount() {
+    this.renderCircles();
+  }
+
+  componentDidUpdate() {
+    this.renderCircles();
+  }
+
+  renderCircles() {
+    let node = this.node;
+    let circle = d3
+      .select(node)
+      .selectAll(`circle`)
+      .data([33, 55, 77]);
+
+    circle
+      .enter()
+      .append('circle')
+      .attr('cy', 60)
+      .attr('cx', function(d, i) {
+        return i * 100 + 30;
+      })
+      .attr('r', function(d) {
+        return Math.sqrt(d);
+      });
+
+    circle.exit().remove();
+  }
+
+  render() {
+    return <svg ref={node => (this.node = node)} width={500} height={500} />;
+  }
+}
+
+export default class PresentationView extends React.Component {
+  render() {
+    return (
+      <div className="presentation-view">
+        <div>Nothing here yet!</div>
+        <div>
+          <Circles data={[32, 64, 128]} />
+        </div>
+        <div>Nothing here yet!</div>
+      </div>
+    );
+  }
+}
