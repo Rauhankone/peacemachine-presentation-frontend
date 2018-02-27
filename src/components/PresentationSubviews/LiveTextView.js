480cf2be1b6e1b9338d3945cf3fc41c28c13f70a
Juhani Åhman <juhani.ahman@digitalentshelsinki.fi>
2018-02-27 15:01:36 +0200

    Add livetext to presentation

--- src/components/PresentationSubviews/LiveTextView.js
+++ src/components/PresentationSubviews/LiveTextView.js
@@ -1,20 +1,11 @@
 import React from 'react';
-
+//        {props.text0}
 export default props => {
   return (
-    <div>
-      <div style={{ width: `500px`, height: `500px`, float: `left` }}>
-        fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds
-        fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds
-      </div>
-      <div style={{ width: `500px`, height: `500px`, float: `left` }}>
-        fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds
-        fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds
-      </div>
-      <div style={{ width: `500px`, height: `500px`, float: `left` }}>
-        fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds fdsfds
-        fdsfds fdsfds fdsfds fdsfds fdsfds
-      </div>
+    <div style={{ width: `500px`, height: `500px`, float: `left` }}>
+      {props.data.map(a => {
+        return <span>{a.transcript}</span>;
+      })}
     </div>
   );
 };
