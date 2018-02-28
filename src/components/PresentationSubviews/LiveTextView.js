f93955fcfdf580359141baa0cc0279ec76e2adec
yliaho <joonas.yliaho@gmail.com>
2018-02-28 15:59:50 +0200

    mm

--- src/components/PresentationSubviews/LiveTextView.js
+++ src/components/PresentationSubviews/LiveTextView.js
@@ -5,6 +5,9 @@ import './LiveTextView.css';
 class LiveTextView extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      channelCols: []
+    };
   }
 
   componentDidMount() {
@@ -15,36 +18,14 @@ class LiveTextView extends React.Component {
     this.createLiveTextView();
   }
 
-  createLiveTextView() {
-    const elem = this.elem;
-
-    let update = d3
-      .select(elem)
-      .select('.col')
-      .selectAll('.sentence')
-      .data(this.props.data);
-
-    update.exit().remove();
-
-    update
-      .enter()
-      .insert('div', ':first-child')
-      .attr('class', 'sentence')
-      .style('top', 0)
-      .text(d => d.transcript);
-
-    update
-      .transition()
-      .duration(500)
-      .style('top', (_, i) => `${(i + 1) * 90}px`);
-  }
+  createLiveTextView() {}
 
   render() {
     return (
       <div className="live-text-view" ref={elem => (this.elem = elem)}>
-        <div className="col" />
-        <div className="col" />
-        <div className="col" />
+        <div className="col">moi</div>
+        <div className="col">moi</div>
+        <div className="col">moi</div>
       </div>
     );
   }
--- src/components/PresentationView.js
+++ src/components/PresentationView.js
@@ -12,42 +12,28 @@ export default class PresentationView extends React.Component {
     this.state = {
       liveTextData: []
     };
+  }
+
+  componentWillMount() {
     this.socket = initSocket('input');
+  }
+
+  componentDidMount() {
     if (this.socket) {
-      this.socket.emit('connected', { viewName: 'presentation' });
       this.socket.on('channelUpdated', data => {
         console.log(data);
         this.setState((prevState, props) => ({
           liveTextData: [data, ...prevState.liveTextData]
         }));
-        console.log(this.state.liveTextData);
+        console.log('sdftu', this.state.liveTextData);
       });
     }
-  }
-
-  componentDidMount() {
-    // this.timer = setInterval(() => {
-    //   this.setState((prevState, props) => ({
-    //     liveTextData: [
-    //       {
-    //         confidence: 1.0,
-    //         id: 'fddfsgfd',
-    //         transcript: `foobar ${Math.random() * 10000} ${Math.random() *
-    //           10000} ${Math.random() * 10000} ${Math.random() *
-    //           10000}${Math.random() * 10000}`
-    //       },
-    //       ...prevState.liveTextData
-    //     ]
-    //   }));
-    // }, 3000);
   }
   render() {
     return (
       <div className="presentation-view">
         <LiveTextView data={this.state.liveTextData} />
-        {
-          //<WordCloudView />
-        }
+        {/* {<WordCloudView />} */}
       </div>
     );
   }
