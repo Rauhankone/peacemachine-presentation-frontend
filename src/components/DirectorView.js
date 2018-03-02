7ce14ef411734c78322335ab893d884eb063abe1
yliaho <joonas.yliaho@gmail.com>
2018-03-02 12:57:32 +0200

    further director style improvements, channel stuff

--- src/components/DirectorView.js
+++ src/components/DirectorView.js
@@ -4,6 +4,7 @@ import { slugify, capitalize } from '../utils';
 
 import socketService from '../services/socket-service';
 import '../styles/DirectorView.css';
+import Icon from '@fortawesome/react-fontawesome';
 
 export default class DirectorView extends React.Component {
   constructor(props) {
@@ -22,7 +23,8 @@ export default class DirectorView extends React.Component {
       'zoom tool',
       'loop'
     ],
-    activeSlide: 'live text'
+    activeSlide: 'live text',
+    appointedChannels: []
   };
 
   directorSocket = () => {
@@ -58,16 +60,34 @@ export default class DirectorView extends React.Component {
     socketService.emitEvent('changeSlide', { slideName: slide });
   };
 
+  handleChannelClick = channel => event => {
+    this.setState(({ appointedChannels }) => {
+      return !appointedChannels.includes(channel)
+        ? {
+            appointedChannels: [...appointedChannels, channel]
+          }
+        : {
+            appointedChannels: [...appointedChannels.filter(c => c !== channel)]
+          };
+    });
+  };
+
   render() {
     return (
       <div className="director-view">
         <div className="director-view-main-grid">
           <div className="grid-item grid-leftSide subgrid-left">
             <div className="grid-sub-item stage-view">
-              <p>Current slide is:</p>
-              <h3>{capitalize(this.state.activeSlide)}</h3>
+              <div className="bubble">
+                <span>Current Slide</span>
+                <h3>{capitalize(this.state.activeSlide)}</h3>
+              </div>
             </div>
             <ul className="grid-sub-item controls">
+              <h3 className="all-slides-title">
+                <Icon icon={['far', 'clone']} className="icon" />
+                Slides
+              </h3>
               {this.state.slides.map(slide => (
                 <li className={`${slugify(slide)}-select control`} key={slide}>
                   <label className="control-radio">
@@ -84,10 +104,25 @@ export default class DirectorView extends React.Component {
               ))}
             </ul>
           </div>
-          <div className="grid-item grid-rightSide subgrid-right">
-            <div className="grid-sub-item connected-list">
-              {this.state.channels.map(ch => <p key={ch.id}>{ch.id}</p>)}
-            </div>
+          <div className="grid-item grid-rightSide main subgrid-right">
+            <ul className="grid-sub-item connected-list">
+              {this.state.channels.map(channel => (
+                <li key={channel.id}>
+                  <div className="channel-control">
+                    <label>
+                      <input
+                        type="checkbox"
+                        checked={this.state.appointedChannels.includes(
+                          channel.id
+                        )}
+                        onChange={this.handleChannelClick(channel.id)}
+                      />
+                    </label>
+                  </div>
+                  <span className="channel-id">{channel.id}</span>
+                </li>
+              ))}
+            </ul>
             <div className="grid-sub-item zoom-tool" />
           </div>
         </div>
