af16247c63fb301d67768a7c7ad2dbbd4fd25243
yliaho <joonas.yliaho@gmail.com>
2018-03-13 16:19:10 +0200

    flash `SentenceSpan` on enter with css

--- src/components/PresentationView/SentenceSpan.jsx
+++ src/components/PresentationView/SentenceSpan.jsx
@@ -4,7 +4,8 @@ import { NodeGroup } from 'react-move';
 
 export default class SentenceSpan extends React.Component {
   state = {
-    letterIndex: 0
+    letterIndex: 0,
+    finished: false
   };
 
   componentDidMount() {
@@ -22,21 +23,25 @@ export default class SentenceSpan extends React.Component {
       }
 
       this.props.onSentenceFinish();
+
+      this.setState({ finished: true });
+
       return clearInterval(INT_ID);
-    }, 16);
+    }, 1000 / 60);
   };
 
   render() {
     return (
       <span
-        className="sentence-span"
+        className={`sentence-span ${
+          this.state.finished ? 'sentence-finished' : null
+        }`}
         style={{
           // opacity: _.clamp(Math.pow(this.props.data.confidence, 5), 0.0, 1)
           opacity: this.props.data.confidence * 0.6 + 0.4 // Original value transformed so that it's between 0.4-1.0
         }}
       >
         {this.props.data.transcript.substring(0, this.state.letterIndex)}
-        &nbsp;
       </span>
     );
   }
