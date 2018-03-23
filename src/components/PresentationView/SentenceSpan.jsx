70b8078a9374f7fbad12f954d9eff71cd14b31cd
Juhani Åhman <juhani.ahman@digitalentshelsinki.fi>
2018-03-23 11:34:57 +0200

    Fix topword highlighting with capitalized words.

--- src/components/PresentationView/SentenceSpan.jsx
+++ src/components/PresentationView/SentenceSpan.jsx
@@ -3,7 +3,6 @@ import _ from 'lodash';
 import { tonesToColor, extractEmotionTones } from './SentimentView';
 
 export default class SentenceSpan extends React.Component {
-
   static OPACITY_TRANSITION_SECONDS = 1;
   static BACKGROUND_TRANSITION_SECONDS = 1;
 
@@ -30,7 +29,9 @@ export default class SentenceSpan extends React.Component {
   };
 
   getBackgroundColor(data) {
-    return (!!data.tones) ? tonesToColor(extractEmotionTones(data)) : 'rgb(100,100,100)';
+    return !!data.tones
+      ? tonesToColor(extractEmotionTones(data))
+      : 'rgb(100,100,100)';
   }
 
   getOpacity() {
@@ -38,29 +39,39 @@ export default class SentenceSpan extends React.Component {
     if (this.props.showConfidence) {
       opacity = _.clamp(Math.pow(this.props.data.confidence, 5), 0.0, 1);
     } else if (this.props.searchTopWord) {
-      opacity = this.props.data.transcript.includes(this.props.topWord) ? 1 : 0.1;
+      opacity = _.includes(
+        _.toLower(this.props.data.transcript),
+        this.props.topWord
+      )
+        ? 1
+        : 0.1;
     }
     return opacity;
   }
 
   render() {
     return (
-      <span id={this.props.data.id+'-wrapper'}
-        className='sentence-span-wrapper'
+      <span
+        id={this.props.data.id + '-wrapper'}
+        className="sentence-span-wrapper"
         style={{
-          transition: `opacity ${SentenceSpan.OPACITY_TRANSITION_SECONDS}s, background ${SentenceSpan.BACKGROUND_TRANSITION_SECONDS}s`,
+          transition: `opacity ${
+            SentenceSpan.OPACITY_TRANSITION_SECONDS
+          }s, background ${SentenceSpan.BACKGROUND_TRANSITION_SECONDS}s`,
           opacity: this.getOpacity(),
-          background: this.props.showIntensity ? this.getBackgroundColor(this.props.data) : 'none'
+          background: this.props.showIntensity
+            ? this.getBackgroundColor(this.props.data)
+            : 'none'
         }}
+      >
+        <span
+          id={this.props.data.id}
+          className={`sentence-span ${
+            this.state.finished ? 'sentence-finished' : null
+          }`}
         >
-          <span
-            id={this.props.data.id}
-            className={`sentence-span ${
-              this.state.finished ? 'sentence-finished' : null
-            }`}
-          >
-            {this.props.data.transcript.substring(0, this.state.letterIndex)}
-          </span>
+          {this.props.data.transcript.substring(0, this.state.letterIndex)}
+        </span>
       </span>
     );
   }
